6fd0f1f709a01013e6144b0b2fa000657d8f8398
張辜 <c24680529@gmail.com>
2025-07-29 14:20:33 +0800

    暴力修改，無論是不是dev都做一樣的domain

--- server/api/index.js
+++ server/api/index.js
@@ -87,7 +87,13 @@ if (serverConfig.sessionStoreType === 'redis') {
 const isDev = process.env.NODE_ENV === 'development';
 
 const options = isDev
-  ? { maxAge: 30 * 86400000 }
+  ? {
+      domain: 'daiski-ispan-is17.vercel.app',
+      maxAge: 30 * 86400000, // session保存30天
+      httpOnly: true, // 無法透過JavaScript讀取
+      secure: true, // HTTPS才能使用
+      sameSite: 'none', // 跨域時也能使用
+    }
   : {
       domain: 'daiski-ispan-is17.vercel.app',
       maxAge: 30 * 86400000, // session保存30天
